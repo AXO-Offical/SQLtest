32b4d4e4b7caeae9fcc6c1e10b34e1a39f7e7201
AXO-Offical <158529134+AXO-Offical@users.noreply.github.com>
2024-03-15 22:57:22 +1000

    Add files via upload

---
+++ LoginDetails.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.0.1
+ */
+
+/**
+ * Database `if0_36167346_EpiXTest`
+ */
+
+/* `if0_36167346_EpiXTest`.`LoginDetails` */
+$LoginDetails = array(
+);
